e5638470dcb7d12c139d0fe10cbb5b51385d9bdd
Taimoor Mumtaz <Taimoor Mumtaz@DESKTOP-ALFST3U>
2023-08-08 12:30:19 +0500

    Add project files.

---
+++ Bulky-Web/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using Bulky_Web.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bulky_Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public CategoryController(ApplicationDbContext)
+        {
+            
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
